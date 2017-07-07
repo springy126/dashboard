e6147ff272d99bd03c9dc650105ac5545eb46794
springy126 <springy126@gmail.com>
2017-07-07 16:37:03 +0800

    to suilt guazi

--- rrd/config.py
+++ rrd/config.py
@@ -1,4 +1,7 @@
 #-*-coding:utf8-*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 # app config
 import os
 LOG_LEVEL = os.environ.get("LOG_LEVEL",'DEBUG')
@@ -26,12 +29,15 @@ ALARM_DB_PASS = os.environ.get("ALARM_DB_PASS","")
 ALARM_DB_NAME = os.environ.get("ALARM_DB_NAME","alarms")
 
 # ldap config
-LDAP_ENABLED = os.environ.get("LDAP_ENABLED",False)
-LDAP_SERVER = os.environ.get("LDAP_SERVER","ldap.forumsys.com:389")
-LDAP_BASE_DN = os.environ.get("LDAP_BASE_DN","dc=example,dc=com")
-LDAP_BINDDN_FMT = os.environ.get("LDAP_BINDDN_FMT","uid=%s,dc=example,dc=com")
-LDAP_SEARCH_FMT = os.environ.get("LDAP_SEARCH_FMT","uid=%s")
-LDAP_ATTRS = ["cn","mail","telephoneNumber"]
+LDAP_ENABLED = os.environ.get("LDAP_ENABLED",True)
+LDAP_SERVER = os.environ.get("LDAP_SERVER","ldap://ldap01.guazi-corp.com:389")
+LDAP_BASE_DN = os.environ.get("LDAP_BASE_DN","ou=技术部,ou=瓜子二手车直卖网,ou=部门,dc=guazi-corp,dc=com")
+#LDAP_BINDDN_FMT = os.environ.get("LDAP_BINDDN_FMT","cn=WikiGetAD,ou=Service management account,dc=guazi-corp,dc=com")
+#LDAP_BINDDN_PASSWORD = os.environ.get("LDAP_BINDDN_PASSWORD","@guazi!123")
+LDAP_BINDDN_FMT = os.environ.get("LDAP_BINDDN_FMT","CN=pwd.srv,OU=Services_Accounts,DC=guazi-corp,DC=com")
+LDAP_BINDDN_PASSWORD = os.environ.get("LDAP_BINDDN_PASSWORD","04dfu@y$7")
+LDAP_SEARCH_FMT = os.environ.get("LDAP_SEARCH_FMT","cn=%s")
+LDAP_ATTRS = ["cn","mail", "mobile"]
 LDAP_TLS_START_TLS = False
 LDAP_TLS_CACERTDIR = ""
 LDAP_TLS_CACERTFILE = "/etc/openldap/certs/ca.crt"
@@ -51,7 +57,7 @@ LANGUAGES   = {
 
 # portal site config
 MAINTAINERS = ['root']
-CONTACT = 'root@open-falcon.org'
+CONTACT = 'wangxueqiang@guazi.com'
 
 try:
     from rrd.local_config import *
